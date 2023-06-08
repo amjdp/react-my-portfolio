3145628b1e96e65fdd681fa67b95601c4839dd94
amjdp <athiramj123@gmail.com>
2023-06-08 16:30:09 +0530

    portfolio starter

---
+++ readme.txt
@@ -0,0 +1,6 @@
+two folders - public & src
+public - index.html - bootstrap cdn 
+index.js 
+index.css - google fonts
+app.js
+assets - icons & images
---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>My Portfolio</h1>
+      <p> Athira M J </p>
+    </div>
+  );
+}
+
+export default App;
